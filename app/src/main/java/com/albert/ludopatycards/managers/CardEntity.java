5f2f81d0edbbeacefa4746c1f97a1a97eefdfd8b
Alberto <al_loki@msn.com>
2017-02-17 00:42:21 +0100

    Add CardEntity to manager

--- app/src/main/java/com/albert/ludopatycards/managers/CardEntity.java
+++ app/src/main/java/com/albert/ludopatycards/managers/CardEntity.java
@@ -1,6 +1,35 @@
 package com.albert.ludopatycards.managers;
 
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
 
 public class CardEntity {
+
+    @SerializedName("remaining") private int remaining;
+    @SerializedName("cards") private List<Cards> cards;
+
+
+    public int getRemaining() {
+        return remaining;
+    }
+
+    public void setRemaining(int remaining) {
+        this.remaining = remaining;
+    }
+
+
+    public class Cards {
+
+        @SerializedName("image") String image;
+
+        public String getImage() {
+            return image;
+        }
+
+        public void setImage(String image) {
+            this.image = image;
+        }
+    }
 }
